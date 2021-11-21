16986a2d7520d75a8ff0528f47ce5e0ef75bb731
pkatre2 <88171729+pkatre2@users.noreply.github.com>
2021-11-21 13:59:16 -0600

    Update Sorting.java

--- src/main/java/joinery/impl/Sorting.java
+++ src/main/java/joinery/impl/Sorting.java
@@ -75,4 +75,29 @@ public class Sorting {
 
         return sorted;
     }
+    
+    public static <V> DataFrame<V> sortByIdx(
+            final DataFrame<V> df) {
+        final DataFrame<V> sorted = new DataFrame<V>(df.columns());
+        final Comparator<Integer> cmp = new Comparator<Integer>() {
+            @Override
+            public int compare(final Integer r1, final Integer r2) {
+                return r1;
+            }
+        };
+
+        final Integer[] rows = new Integer[df.length()];
+        for (int r = 0; r < df.length(); r++) {
+            rows[r] = r;
+        }
+        Arrays.sort(rows, cmp);
+
+        final List<Object> labels = new ArrayList<>(df.index());
+        for (final Integer r : rows) {
+            final Object label = r < labels.size() ? labels.get(r) : r;
+            sorted.append(label, df.row(r));
+        }
+
+        return sorted;
+    }
 }
